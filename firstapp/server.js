76c159dc63f3d695ecfeaee76b60b56089dddaf8
smurciac <santiago.murcia@globant.com>
2017-12-26 08:38:21 -0500

    Add files via upload

--- firstapp/server.js
+++ firstapp/server.js
@@ -4,9 +4,9 @@ var url = require("url");
 function start(route, handle) {
   function onRequest(request, response) {
     var pathname = url.parse(request.url).pathname;
-    console.log("Request for " + pathname + " recieved.");
+    console.log("Request for " + pathname + " received.");
 
-    route(handle, pathname, response);
+    route(handle, pathname, response, request);
   }
 
   http.createServer(onRequest).listen(8888);
